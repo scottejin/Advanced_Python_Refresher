25c9409a8a1c59b27cd6073bda474c7cfbf73f9d
638374 <638374@trinity.vic.edu.au>
2026-02-04 12:06:50 +1100

    Grid expansion, making it 15 x 8

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 #Make Grid
 grid = [
-    ["" for _ in range(10)],
-    ["" for _ in range(10)],
-    ["" for _ in range(10)],
-    ["" for _ in range(10)],
-    ["" for _ in range(10)],
-    ["" for _ in range(10)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
+    ["" for _ in range(15)],
 ]
 def setup_grid():
-    for i in range(6):
-        for j in range(10):
+    for i in range(8):
+        for j in range(15):
             grid[i][j] = "."
     grid[0][0] = "S"  # Start
-    grid[5][9] = "E"  # End
+    grid[7][14] = "E"  # End
 setup_grid()
 for row in grid:
     print(row)
