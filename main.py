ff96ecff186c215c5d9d567f3a4719eb11846307
638374 <638374@trinity.vic.edu.au>
2026-02-04 11:46:49 +1100

    Starting simple table

---
+++ main.py
@@ -0,0 +1,19 @@
+#Make Grid
+grid = [
+    ["" for _ in range(10)],
+    ["" for _ in range(10)],
+    ["" for _ in range(10)],
+    ["" for _ in range(10)],
+    ["" for _ in range(10)],
+    ["" for _ in range(10)],
+]
+def setup_grid():
+    for i in range(6):
+        for j in range(10):
+            grid[i][j] = "."
+    grid[0][0] = "S"  # Start
+    grid[5][9] = "E"  # End
+setup_grid()
+for row in grid:
+    print(row)
+
