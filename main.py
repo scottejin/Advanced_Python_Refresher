0455dd3356193b50fde19bf1fb1b2e58ba12bf9f
638374 <638374@trinity.vic.edu.au>
2026-02-05 09:04:21 +1100

    Expand grid dimensions to 25 x 15 and update start/end positions

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #Make Grid
-grid = [["" for _ in range(15)] for _ in range(8)]
+grid = [["" for _ in range(25)] for _ in range(15)]
 def setup_grid():
-    for i in range(8):
-        for j in range(15):
+    for i in range(15):
+        for j in range(25):
             grid[i][j] = "."
     grid[0][0] = "S"  # Start
-    grid[7][14] = "E"  # End
+    grid[14][24] = "E"  # End
 setup_grid()
 for row in grid:
     print(row)
 
 #Making the algorithm
+
 
