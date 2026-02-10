563274aab08e256d8bb86abb7075f1280439132e
scottejin <638374@trinity.vic.edu.au>
2026-02-10 18:41:24 +1100

    Improve code readability with comments and variable naming in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random as rnd
 
 # Grid dimensions
-ROWS = 30
-COLS = 35
+ROWS = 30 # sets row
+COLS = 35 # sets columns ....
 
 #Make Grid
 grid = [["" for _ in range(COLS)] for _ in range(ROWS)]
@@ -21,7 +21,7 @@ def run_a_star(grid):
     closed_list = []
 
     starting_node = {
-        'position': (0,0),   # Starting pos ig?
+        'position': (0,0),   # Starting pos
         'actual_cost': 0,  # Cost so far
         'heuristic_cost': abs(0-(ROWS - 1)) + abs(0-(COLS - 1)), # Goal distance as a absolute value
         'total_cost': abs(0-(ROWS - 1)) + abs(0-(COLS - 1)), # How much distance still needed
@@ -56,29 +56,29 @@ def run_a_star(grid):
             while current is not None: # Checking if the parent position is existent or not after running the loop
                 path.append(current['position']) #Appends the current loops position to path
                 current = current['parent'] # Resetting path
-            path.reverse()
-            found_path = path
+            path.reverse() # reverse as this is now the end
+            found_path = path #saving to global variable
             break
 
 
     # 7)   Find all neighbors of current (up, down, left, right, etc.). Call them children.
-        current_row, current_col = current['position']
-        neighbours =[]
+        current_row, current_col = current['position'] # saving (x,y) to x y
+        neighbours =[] #neighbours to current
 
 
         # You can move Up/down/left/right
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
         for movable_directions in directions:
-            new_row = current_row + movable_directions[0]
-            new_col = current_col + movable_directions[1]
+            new_row = current_row + movable_directions[0] #down up
+            new_col = current_col + movable_directions[1] # Left right
 
-            if 0 <= new_row < ROWS and 0 <= new_col < COLS:
+            if 0 <= new_row < ROWS and 0 <= new_col < COLS: # if they are in the grid
                 if grid[new_row][new_col] in['.','E']:
                     neighbour_position = (new_row, new_col)
                     already_checked = any(node['position'] == neighbour_position for node in closed_list)
 
-                    if not already_checked:
-                        neighbours.append(neighbour_position)
+                    if not already_checked: # if not in already checked
+                        neighbours.append(neighbour_position) # append tgo positions in neighbour
 
 
         for neighbour in neighbours:
@@ -117,9 +117,8 @@ if user_seed.strip().isdigit(): #Needed some ASSISTANCE to do this, i had no ide
     rnd.seed(int(user_seed))
 
 obstacles = 0
-final_path = None
-# Limit obstacles to half the grid plus one full row.
-max_obstacles = (ROWS * COLS) // 2 + ROWS
+final_path = None #temp var
+max_obstacles = (ROWS * COLS) // 2 + ROWS # Makes obstacle generation cooler, serious talk == better as they scale to the requirements
 while obstacles < max_obstacles:
     empty_cells = [
         (i, j)
@@ -130,34 +129,27 @@ while obstacles < max_obstacles:
     if not empty_cells:
         break
 
-    r, c = rnd.choice(empty_cells)
+    r, c = rnd.choice(empty_cells) #picking random to assign obstacle
     grid[r][c] = "x"
 
-    final_path = run_a_star(grid)
+    final_path = run_a_star(grid) #running the final path to retrieve result
     if final_path is None:
         grid[r][c] = "."  # Remove the obstacle that blocked the path
         continue
 
-    obstacles += 1
+    obstacles += 1 # adding one to sum obstacle count
 
 final_path = run_a_star(grid)
 if final_path is None:
-    print("No path found.")
+    print("No path found.") # Won't happen, but just to tick off an optional task in teams
 else:
-    for r, c in final_path:
+    for r, c in final_path: # setting the variables to improve readability '#' and  '*' was bad
         if grid[r][c] == ".":
             grid[r][c] = "@"
     for row in grid:
-        print(row)
+        print(row) # showing result
 
-# For Mrs.F, attached is the website link for the following souurce...
-
-# https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2 -> Website used for advice
-
-# A* pathfinding notes (simple, step-by-step):
-
-
-#BELOW IS MY ORIGIONAL CODE BEFORE USING FUNCTIONS
+#BELOW IS MY ORIGINAL CODE BEFORE USING FUNCTIONS
 
 # 1) Make two lists: open_list (nodes to check) and closed_list (nodes already checked).
 #
